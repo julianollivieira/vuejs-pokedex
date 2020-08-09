a1582a16e730d8579b12daa2f64c902e8d13e469
Julian <julian.ollivieira@gmail.com>
2020-08-09 15:54:36 +0200

    Add pokemon list
    Addde pokemon list and API call.
    Added some fonts.
    Cleanup the css file.

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ Vue.component('pkmn-img', {
 function ucfirst(string){
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
 
 var app = new Vue({
   el: '#app',
   data: {
-    pokemon_id: 1,
+    // pokemon_id: 1,
+    all_pokemon: [],
     pokemon: {
       sprites: {
         front_default: "",
@@ -18,11 +24,23 @@ var app = new Vue({
     },
   },
   methods: {
-    async getPokemon(){
+    async getAllPokemon(){
+      let resp = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+      console.log(resp);
+      
+      resp.data.results.forEach(element => {
+        element.name = ucfirst(element.name);
+      });
+
+      this.all_pokemon = resp.data.results;
+    },
+    async getPokemon(index){
+      index = index.toLowerCase();
       try {
         // API Request
-        let resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemon_id}`);
+        let resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`);
         resp.data.name = ucfirst(resp.data.name);
+        resp.data.id = pad(resp.data.id, 3);
         this.pokemon = resp.data;
 
         // Get dominant color for pokemon sprite
@@ -48,6 +66,7 @@ var app = new Vue({
     },
   },
   beforeMount(){
-    this.getPokemon(this.pokemon_id);
+    this.getAllPokemon();
+    this.getPokemon('Bulbasaur');
   },
 });
